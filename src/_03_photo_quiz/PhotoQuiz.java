f437fe75aa5d528880ee64884dfe18ccdfe5e938
Andrew Wasicek <awasicek@gmail.com>
2019-12-30 16:40:46 -0800

    completed photo quiz and drum kit

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -5,6 +5,8 @@ package _03_photo_quiz;
  */
 
 import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -26,35 +28,70 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
+		String imageAddress = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzaG-OtpYZEtWRO-CdocWQWWxIBf6obcdu3ebneZXuLKDnHGHq&s";
 
 		// 2. create a variable of type "Component" that will hold your image
+		Component imageHolder;
 
 		// 3. use the "createImage()" method below to initialize your Component
+		imageHolder = createImage(imageAddress);
 
 		// 4. add the image to the quiz window
+		quizWindow.add(imageHolder);
 
 		// 5. call the pack() method on the quiz window
+		quizWindow.pack();
 
 		// 6. ask a question that relates to the image
+		String userAnswer = JOptionPane.showInputDialog("Philosopher or tiger?");
 
 		// 7. print "CORRECT" if the user gave the right answer
-
-		// 8. print "INCORRECT" if the answer is wrong
+		if (userAnswer.equalsIgnoreCase("tiger")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		} else {
+			// 8. print "INCORRECT" if the answer is wrong
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+		}
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+		quizWindow.remove(imageHolder);
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
+		String otherImageAddress = "https://comicvine1.cbsistatic.com/uploads/scale_small/0/9116/620154-calvin2.jpg";
+		Component otherImageHolder = createImage(otherImageAddress);
 
+		MouseAdapter adapter = new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				JOptionPane.showMessageDialog(null, "Hey. Don't click me!");
+			}
+			public void mouseMoved(MouseEvent e) {
+				System.out.println("x: " + e.getX() + ", y: " + e.getY());
+			}
+		};
+		
+		otherImageHolder.addMouseListener(adapter);
+		
+		otherImageHolder.addMouseMotionListener(adapter);
+		
 		// 11. add the second image to the quiz window
+		quizWindow.add(otherImageHolder);
 
 		// 12. pack the quiz window
+		quizWindow.pack();
 
 		// 13. ask another question
+		userAnswer = JOptionPane.showInputDialog("Friend or food for the tiger?");
+
 
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (userAnswer.equalsIgnoreCase("friend")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+		} else {
+			// 8. print "INCORRECT" if the answer is wrong
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -20,22 +20,39 @@ import javax.swing.JPanel;
 
 public class DrumKit implements MouseListener {
 
-	JLabel drumLabelWithImage;
+	private final int FRAME_WIDTH = 800;
+	private final int FRAME_HEIGHT = 600;
+	private final String DRUM_FILENAME = "snare.jpg";
+	private final String DRUM_SOUND_FILENAME = "drum.wav";
+	private final String CYMBAL_FILENAME = "cymbal.jpg";
+	private final String CYMBAL_SOUND_FILENAME = "cymbal.wav";
+	
+	private JFrame frame;
+	private JPanel panel;
+	private JLabel drumLabelWithImage;
+	private JLabel cymbalLabelWithImage;
 
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
+		frame = new JFrame();
 
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 3. Set the size of the frame
+		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 
 		// 4. Set the title of the frame
+		frame.setTitle("Drum Kit v1.0");
 
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
+		panel = new JPanel();
 
 		// 6. Add the panel to the frame. (The panel is invisible.)
+		frame.add(panel);
 
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
@@ -44,15 +61,23 @@ public class DrumKit implements MouseListener {
 
 		// 9. Edit the next line to use your String variable
 		// drumLabelWithImage = createLabelImage(drumImageString);
+		drumLabelWithImage = createLabelImage(DRUM_FILENAME);
+		cymbalLabelWithImage = createLabelImage(CYMBAL_FILENAME);
 
 		// 10. Add the image to the panel
+		panel.add(drumLabelWithImage);
+		panel.add(cymbalLabelWithImage);
 
 		// 11. Set the layout of the panel to "new GridLayout()"
+		panel.setLayout(new GridLayout());
 
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
+		frame.pack();
 
 		// 13. add this mouse listener to drumLabelWithImage
+		drumLabelWithImage.addMouseListener(this);
+		cymbalLabelWithImage.addMouseListener(this);
 
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
@@ -62,8 +87,9 @@ public class DrumKit implements MouseListener {
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
+		System.out.println("mouse clicked");
 
-		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
+		JLabel imageClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
 
@@ -72,6 +98,13 @@ public class DrumKit implements MouseListener {
 		// leagueofamazing/code4life.
 
 		// 16. If they clicked on the drumImage...
+		if (imageClicked == drumLabelWithImage) {
+			System.out.println("clicked the drum");
+			playSound(DRUM_SOUND_FILENAME);
+		} else if (imageClicked == cymbalLabelWithImage) {
+			System.out.println("clicked the cymbal");
+			playSound(CYMBAL_SOUND_FILENAME);
+		}
 
 		// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
